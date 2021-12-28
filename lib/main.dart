0971f7a66d38da55e10f7925c828c19817774bf7
udohahanor <udohahanor@gmail.com>
2021-12-28 11:11:14 +0100

    desktop_window

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
+
+import 'package:desktop_window/desktop_window.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:nike_store/screens/screens.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb && (Platform.isWindows || Platform.isMacOS || Platform.isLinux)) {
+    await DesktopWindow.setMinWindowSize(const Size(600, 1024));
+  }
   runApp(const MainPage());
 }
 
