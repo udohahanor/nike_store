ec104dae19e57033b37ca7364deb01c60caad252
udohahanor <udohahanor@gmail.com>
2022-03-17 09:36:37 +0100

    badge

--- lib/widgets/navicons.dart
+++ lib/widgets/navicons.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
 class NavIcons extends StatelessWidget {
@@ -8,14 +9,23 @@ class NavIcons extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Row(
-        children: const [
-          Icon(
-            Icons.shopping_cart,
-            color: Colors.black,
-            size: 30.0,
+        children: [
+          Badge(
+            badgeColor: Colors.black,
+            badgeContent: const Text(
+              '4',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            child: const Icon(
+              Icons.shopping_cart,
+              color: Colors.black,
+              size: 30.0,
+            ),
           ),
-          SizedBox(width: 30.0),
-          Icon(
+          const SizedBox(width: 30.0),
+          const Icon(
             Icons.search,
             color: Colors.black,
             size: 30.0,
